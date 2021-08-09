38c4a3656f0456f8bc0468aaf7d15139a9a63fc7
longvt <longvu2000.hust@gmail.com>
2021-08-10 00:30:00 +0700

    Saving goal UI add

--- lib/main_screen/home_screen/saving_goals/add_record_screen.dart
+++ lib/main_screen/home_screen/saving_goals/add_record_screen.dart
@@ -17,40 +17,83 @@ class _AddRecordScreenState extends State<AddRecordScreen> {
   TextEditingController note = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    final curScaleFactor = MediaQuery.of(context).textScaleFactor;
+
     return Scaffold(
+        appBar: AppBar(
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontFamily: 'Inter',
+              color: Color(0xffecf9f4),
+              fontSize: 16 * curScaleFactor,
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.normal,
+            ),
+            title: Text("Nhật ký")),
         body: Container(
       padding: EdgeInsets.only(top: 30),
       child: Form(
         key: _formKey,
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                    onPressed: () {},
-                    child: Icon(Icons.arrow_back_ios_outlined)),
-                Text('Nhật ký'),
-                ElevatedButton(onPressed: () {}, child: Text('Lưu'))
-              ],
-            ),
             Container(
+              width: MediaQuery.of(context).size.width / 187.5 * 171.5,
+              height: MediaQuery.of(context).size.height / 333.5 * 46,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12)
+              ),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text('Số tiền'),
-                  TextFormField(
-                    controller: money,
-                    validator: (value) {
-                      if (double.tryParse(value!) == null)
-                        return 'Dữ liệu k hợp lệ';
-                      return null;
-                    },
+                  Container(
+                    width: MediaQuery.of(context).size.width / 187.5 * 89,
+                    height: MediaQuery.of(context).size.height / 333.5 * 8,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 187.5 * 155.5,
+                    child: Text('Số tiền',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          fontFamily: 'Inter',
+                          color: Color(0xff1a1a1a),
+                          fontSize: 12 * curScaleFactor,
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                        )),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 187.5 * 155.5,
+                    child: TextFormField(
+                      controller: money,
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontFamily: 'Inter',
+                        color: Color(0xff00865d),
+                        fontSize: 32 * curScaleFactor,
+                        fontWeight: FontWeight.w600,
+                        fontStyle: FontStyle.normal,
+                      ),
+                      validator: (value) {
+                        if (double.tryParse(value!) == null)
+                          return 'Dữ liệu k hợp lệ';
+                        return null;
+                      },
+                    ),
                   )
                 ],
               ),
             ),
             Container(
+              width: MediaQuery.of(context).size.width / 187.5 * 89,
+              height: MediaQuery.of(context).size.height / 333.5 * 4,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 187.5 * 171.5,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12)
+              ),
               child: Column(
                 children: [
                   Row(
@@ -72,69 +115,70 @@ class _AddRecordScreenState extends State<AddRecordScreen> {
                             decoration: InputDecoration(hintText: 'Ghi chú'),
                           ))
                     ],
-                  )
+                  ),
+                  GestureDetector(
+                    onTap: () => {
+                      DatePicker.showDatePicker(context,
+                          showTitleActions: true, minTime: DateTime.now(),
+                          // maxTime: DateTime(2019, 6, 7),
+                          onChanged: (date) {
+                            print(DateFormat.yMMMMEEEEd().format(date));
+                          }, onConfirm: (date) {
+                            // print('confirm $date');
+                            setState(() {
+                              time = date;
+                            });
+                          }, currentTime: time, locale: LocaleType.vi)
+                    },
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: Container(
+                              padding:
+                              EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.only(right: 26),
+                                    child: Icon(
+                                      Icons.calendar_today_outlined,
+                                      color: Color(0xff12B281),
+                                    ),
+                                  ),
+                                  Text(
+                                    DateFormat.yMMMMEEEEd().format(time),
+                                    style: TextStyle(color: Color(0xff12B281)),
+                                  )
+                                ],
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-            GestureDetector(
-              onTap: () => {
-                DatePicker.showDatePicker(context,
-                    showTitleActions: true, minTime: DateTime.now(),
-                    // maxTime: DateTime(2019, 6, 7),
-                    onChanged: (date) {
-                  print(DateFormat.yMMMMEEEEd().format(date));
-                }, onConfirm: (date) {
-                  // print('confirm $date');
-                  setState(() {
-                    time = date;
-                  });
-                }, currentTime: time, locale: LocaleType.vi)
-              },
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 187.5 * 171.5,
+                  height: MediaQuery.of(context).size.height / 333.5 * 24,
+                  margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height / 333.5 * 16,),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          Map data = {
+                            'money': double.parse(money.text),
+                            'note': note.text,
+                            'time': Timestamp.fromDate(time)
+                          };
+                          print(data);
+                          Navigator.pop(context, data);
+                        }
+                      },
+                      child: Text('LƯU'.toUpperCase())),
                 ),
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(right: 26),
-                            child: Icon(
-                              Icons.calendar_today_outlined,
-                              color: Color(0xff12B281),
-                            ),
-                          ),
-                          Text(
-                            DateFormat.yMMMMEEEEd().format(time),
-                            style: TextStyle(color: Color(0xff12B281)),
-                          )
-                        ],
-                      ),
-                    ))
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    Map data = {
-                      'money': double.parse(money.text),
-                      'note': note.text,
-                      'time': Timestamp.fromDate(time)
-                    };
-                    print(data);
-                    Navigator.pop(context, data);
-                  }
-                },
-                child: Text('Lưu'),
               ),
             )
           ],
--- lib/main_screen/home_screen/saving_goals/record_day_widget.dart
+++ lib/main_screen/home_screen/saving_goals/record_day_widget.dart
@@ -8,13 +8,28 @@ class RecordDayWidget extends StatelessWidget {
   final NumberFormat formatter = NumberFormat('###,###,###,##0.##');
   @override
   Widget build(BuildContext context) {
+    final curScaleFactor = MediaQuery.of(context).textScaleFactor;
     return Container(
       padding: EdgeInsets.only(top: 10),
       child: Column(
         children: [
           Text(DateFormat.yMMMMEEEEd().format(record['time'].toDate())),
-          Text(formatter.format(record['money'])),
-          Text(record['note']),
+          Text(formatter.format(record['money']),
+              style: TextStyle(
+                fontFamily: 'Inter',
+                color: Color(0xff1a1a1a),
+                fontSize: 18 * curScaleFactor,
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.normal,
+              )),
+          Text(record['note'],
+              style: TextStyle(
+                fontFamily: 'Inter',
+                color: Color(0xff999999),
+                fontSize: 14 * curScaleFactor,
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.normal,
+              )),
         ],
       ),
     );
